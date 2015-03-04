aeaf6199664f1229d4f57ec0e987d690d828e889
Alex Huang <yihsihuang@gmail.com>
2015-03-04 05:15:31 -0800

    Add return backspace support.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -72,38 +72,60 @@ static void tty_do_read(TTY* p_tty)
     }
 }
 
+static void put_key_to_buffer(TTY* p_tty, unsigned int key)
+{
+    if(p_tty->inbuf_count < TTY_BUFFER_SIZE)
+    { 
+        *(p_tty->p_inbuf_head)=key;
+        p_tty->p_inbuf_head++;
+        if(p_tty->p_inbuf_head == p_tty->in_buf + TTY_BUFFER_SIZE)
+             p_tty->p_inbuf_head = p_tty->in_buf;
+        p_tty->inbuf_count++;
+    }
+}
+
 void in_process(TTY* p_tty, unsigned int key)
 {
         char output[2] = {'\0', '\0'};
 
-        if (!(key & FLAG_EXT)) {
-		if (p_tty->inbuf_count < TTY_BUFFER_SIZE) {
+        if (!(key & FLAG_EXT)) 
+        {
+             put_key_to_buffer(p_tty, key);
+		/*if (p_tty->inbuf_count < TTY_BUFFER_SIZE) {
 		    *(p_tty->p_inbuf_head) = key;
 		    p_tty->p_inbuf_head++;
 		    if (p_tty->p_inbuf_head == p_tty->in_buf + TTY_BUFFER_SIZE){
 			p_tty->p_inbuf_head = p_tty->in_buf;
 		    }
 		    p_tty->inbuf_count++;
-		}
+		}*/
         }
-        else {
-                int raw_code = key & 0x01FF;
-                switch(raw_code) {
-                case UP:
-                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
-                                asm volatile("cli");
+        else 
+        {
+               int raw_code = key & 0x01FF;
+               switch(raw_code) {
+               case ENTER:
+                    put_key_to_buffer(p_tty,'\n'); 
+                    break;
+               case BACKSPACE:
+                    put_key_to_buffer(p_tty,'\b');
+                    break; 
+               case UP:
+                    if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
+                               /* asm volatile("cli");
                                 outb(0x3D4, 0xC);
                                 outb(0x3D5, ((80*15) >> 8) & 0xFF);
                                 outb(0x3D4, 0xD);
                                 outb(0x3D5, (80*15) & 0xFF);
-                                asm volatile("sti");
-                        }
-                        break;
+                                asm volatile("sti");*/
+                               scroll_screen(p_tty->p_console, SCR_DN);
+                     }
+                     break;
                 case DOWN:
-                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
-				/* Shift+Down, do nothing */
-                        }
-                        break;
+                     if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
+                               scroll_screen(p_tty->p_console, SCR_UP);
+                     }
+                     break;
                 case F1:
                 case F2:
                 case F3:
@@ -116,11 +138,11 @@ void in_process(TTY* p_tty, unsigned int key)
                 case F10:
                 case F11:
                 case F12:
-                    if((key & FLAG_ALT_L)|| (key & FLAG_ALT_R))
-                    {
+                     if((key & FLAG_ALT_L)|| (key & FLAG_ALT_R))
+                     {
                         select_console(raw_code-F1);
-                    }
-                    break;  
+                     }
+                     break;  
                 default:
                         break;
                 }
